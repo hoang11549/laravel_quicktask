2bb683756c5f7a8d28ba308daad81cec9c0b0d89
XuanHoang <lexuanhoang1912@gmail.com>
2021-07-07 16:40:22 +0700

    Fix Add Task Function

---
+++ resources/lang/vi/messages.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'TaskList' => 'Danh  Sách',
+    'AddTask' => 'Thêm Nhiệm Vụ',
+    'Tasks' => 'Nhiệm Vụ',
+    'NewTask' => 'Thêm Nhiệm Vụ',
+    'Language' => 'Ngôn Ngữ',
+    'English' => 'Tiếng Anh',
+    'Vietnamese' => 'Tiếng Việt',
+    'CurrentTasks' => 'Nhiệm Vụ Đang Có',
+    'Delete ' => 'Xóa',
+    'Task' => 'Task',
+    'Whoops!Somethingwentwrong!' => 'Co gi do sai sai ',
+    'nameErr' => 'Chua nhap ten kia ',
+];
